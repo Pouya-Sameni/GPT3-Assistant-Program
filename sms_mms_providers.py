2cc34d1126d1cadd2fe0b13ccf4cf6895f79da22
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2022-12-18 01:13:33 -0500

    update

---
+++ sms_mms_providers.py
@@ -0,0 +1,9 @@
+PROVIDERS = {
+    "Telus": {"sms": "msg.telus.com", "mms": "mms.att.net", "mms_support": True},
+
+    "Rogers": {"sms": "pcs.rogers.com", "mms": "mms.att.net", "mms_support": True},
+
+    "Bell": {"sms": "txt.bell.ca", "mms_support" : True},
+
+    "Koodo " : {"sms": "msg.koodomobile.com", "mms_support" : True}
+}
